62c0699f67adf25af872e47a7947e6c70cf84a8d
kazurei <167180368+kazurei@users.noreply.github.com>
2026-05-11 09:46:35 +0000

    ああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write("Google Play Store のレビューを取得してCSV保存できます�
 # 入力欄
 app_id = st.text_input(
     "アプリID",
-    value=jp.co.mixi.monsterstrike
+    value=co.mixi.monsterstrike
 )
 
 review_count = st.number_input(
