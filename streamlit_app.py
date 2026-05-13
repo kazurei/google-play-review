5d336cec3304168e0c22440e55589e88cdf7f51b
kazurei <167180368+kazurei@users.noreply.github.com>
2026-05-13 08:25:16 +0000

    hoyoba

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write("Google Play Store のレビューを取得してCSV保存できます�
 # 入力欄
 app_id = st.text_input(
     "アプリID",
-    value="com.sega.pjsekai"
+    value="com.HoYoverse.Nap"
 )
 
 review_count = st.number_input(
