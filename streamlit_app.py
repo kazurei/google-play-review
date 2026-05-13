c804e2b6a30f7eea2558f1dc4d35d885d877bcf6
kazurei <167180368+kazurei@users.noreply.github.com>
2026-05-13 09:21:53 +0000

    fgo

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write("Google Play Store のレビューを取得してCSV保存できます�
 # 入力欄
 app_id = st.text_input(
     "アプリID",
-    value="net.wrightflyer.anothereden"
+    value="com.aniplex.fategrandorder"
 )
 
 review_count = st.number_input(
