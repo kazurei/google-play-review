f6f425282d9dc6334b257c930d8a5e36cd0e18d8
kazurei <167180368+kazurei@users.noreply.github.com>
2026-05-13 09:01:13 +0000

    しゅうううｓり

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write("Google Play Store のレビューを取得してCSV保存できます�
 # 入力欄
 app_id = st.text_input(
     "アプリID",
-    value="value="net.wrightflyer.anothereden""
+    value="net.wrightflyer.anothereden"
 )
 
 review_count = st.number_input(
