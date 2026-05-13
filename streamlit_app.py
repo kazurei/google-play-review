172a59eb5dd2f3ba423b8b1ff9cb6ee3ca0762e4
kazurei <167180368+kazurei@users.noreply.github.com>
2026-05-13 09:00:06 +0000

    アナザーエデン

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write("Google Play Store のレビューを取得してCSV保存できます�
 # 入力欄
 app_id = st.text_input(
     "アプリID",
-    value="com.HoYoverse.Nap"
+    value="value="net.wrightflyer.anothereden""
 )
 
 review_count = st.number_input(
